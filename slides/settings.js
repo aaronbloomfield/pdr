91c27ffae1f60b3d0f3c6aee6f69caa3bf5906a6
Aaron Bloomfield <aaron@virginia.edu>
2013-12-29 22:31:13 -0500

    Figured out why my code was not highlighting correctly: all the plugin paths were wrong...

--- slides/settings.js
+++ slides/settings.js
@@ -16,11 +16,11 @@ Reveal.initialize({
       
       // Optional libraries used to extend on reveal.js
       dependencies: [
-		     { src: 'lib/js/classList.js', condition: function() { return !document.body.classList; } },
-		     { src: 'plugin/markdown/marked.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
-		     { src: 'plugin/markdown/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
-		     { src: 'plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } },
-		     { src: 'plugin/zoom-js/zoom.js', async: true, condition: function() { return !!document.body.classList; } },
-		     { src: 'plugin/notes/notes.js', async: true, condition: function() { return !!document.body.classList; } }
+		     { src: 'reveal.js/lib/js/classList.js', condition: function() { return !document.body.classList; } },
+		     { src: 'reveal.js/plugin/markdown/marked.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+		     { src: 'reveal.js/plugin/markdown/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+		     { src: 'reveal.js/plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } },
+		     { src: 'reveal.js/plugin/zoom-js/zoom.js', async: true, condition: function() { return !!document.body.classList; } },
+		     { src: 'reveal.js/plugin/notes/notes.js', async: true, condition: function() { return !!document.body.classList; } }
 		     ]
       });
