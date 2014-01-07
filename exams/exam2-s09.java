fbc907cb39c2fbfc0471c853b9e433cab39afe9d
Aaron Bloomfield <aaron@virginia.edu>
2014-01-07 11:14:19 -0500

    Added all the past exams as PDFs

---
+++ exams/exam2-s09.java
@@ -0,0 +1,13 @@
+public class Exam2 {
+    public static void main (String[] args) {
+	int tableSize = 9;
+	System.out.println ("Table size: " + tableSize);
+	System.out.println ("Briana: " + "Briana".hashCode() % tableSize);
+	System.out.println ("Dan: " + "Dan".hashCode() % tableSize);
+	System.out.println ("Jeff: " + "Jeff".hashCode() % tableSize);
+	System.out.println ("John: " + "John".hashCode() % tableSize);
+	System.out.println ("Josh: " + "Josh".hashCode() % tableSize);
+	System.out.println ("Randolph: " + "Randolph".hashCode() % tableSize);
+	System.out.println ("Wil: " + "Wil".hashCode() % tableSize);
+    }
+}
