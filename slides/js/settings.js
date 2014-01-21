25bbb1e38a58d0dcbd69e2b7820db75a13cf1a5c
Aaron Bloomfield <aaron@virginia.edu>
2014-01-21 00:55:09 -0500

    Enabled math equations via MathJax (see the last slide of canvas.html for an example of how to render formulas)

--- slides/js/settings.js
+++ slides/js/settings.js
@@ -21,6 +21,8 @@ Reveal.initialize({
 		     { src: 'reveal.js/plugin/markdown/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
 		     { src: 'reveal.js/plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } },
 		     { src: 'reveal.js/plugin/zoom-js/zoom.js', async: true, condition: function() { return !!document.body.classList; } },
-		     { src: 'reveal.js/plugin/notes/notes.js', async: true, condition: function() { return !!document.body.classList; } }
+		     { src: 'reveal.js/plugin/notes/notes.js', async: true, condition: function() { return !!document.body.classList; } },
+		     { src: 'reveal.js/plugin/math/math.js', async: true }
 		     ]
+
       });
