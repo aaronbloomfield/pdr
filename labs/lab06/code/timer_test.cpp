aec4570ca46d83a0ae92eda94bf668246b75ed3e
Winston Liu <Wliu1402@gmail.com>
2020-02-25 00:08:46 -0500

    Modernize the helper files
    - Replace obsolescent gettimeofday with C++11's std::chrono
    - Use strings throughout rather than char*s

--- labs/lab06/code/timer_test.cpp
+++ labs/lab06/code/timer_test.cpp
@@ -6,7 +6,7 @@ int main (void) {
     timer t;
     t.start();
     // do something that takes some time...
-    for ( int i = 0; i < 1000000000; i++ );
+    for (int i = 0; i < 1000000000; i++) {}
     t.stop();
     cout << t << endl;
     cout << t.getTime() << endl;
