6d2bcabb51e605408ebaa1ac6ec6f0dd1865274f
Winston Liu <Wliu1402@gmail.com>
2020-04-09 20:40:23 -0400

    C++-ify fileio.cpp

--- labs/lab10/fileio.cpp
+++ labs/lab10/fileio.cpp
@@ -1,48 +1,51 @@
-// This program shows how C-based file I/O works.  It will print a
-// file to the screen two times.
+// This program shows how C++-based file I/O works.
+// It will print a file to the screen two times.
 
 // included so we can use cout
 #include <iostream>
-// stdlib.h is where exit() lives
-#include <stdlib.h>
+// file I/O
+#include <fstream>
+// cstdlib is where exit() lives
+#include <cstdlib>
 
 using namespace std;
 
-// include the standard I/O library
-#include <stdio.h>
-
 // we want to use parameters
-int main (int argc, char **argv) {
+int main(int argc, char** argv) {
     // verify the correct number of parameters
-    if ( argc != 2 ) {
+    if (argc != 2) {
         cout << "Must supply the input file name as the one and only parameter" << endl;
         exit(1);
     }
-    // attempt to open the supplied file.  FILE is a type designed to
-    // hold file pointers.  The first parameter to fopen() is the
-    // filename.  The second parameter is the mode -- "r" means it
-    // will read from the file.
-    FILE *fp = fopen(argv[1], "r");
-    // if the file wasn't found, output and error message and exit
-    if ( fp == NULL ) {
-        cout << "File '" << argv[1] << "' does not exist!" << endl;
+
+    // attempt to open the supplied file
+    // ifstream stands for "input file stream"
+    ifstream file(argv[1]);
+    // if the file wasn't found, output an error message and exit
+    if (!file.is_open()) {
+        cout << "Unable to open '" << argv[1] << "' for reading" << endl;
         exit(2);
     }
-    // read in each character, one by one.  Note that the fgetc() will
-    // read in a single character from a file, and returns EOF when it
-    // reaches the end of a file.
+
+    // read in characters one by one, until reading fails (we hit the end of the file)
     char g;
-    while ( (g = fgetc(fp)) != EOF )
+    while (file.get(g)) {
         cout << g;
+    }
+
     // a nice pretty separator
     cout << "----------------------------------------" << endl;
-    // rewinds the file pointer, so that it starts reading the file
-    // again from the beginning
-    rewind(fp);
-    // read the file again, and print to the screen
-    while ( (g = fgetc(fp)) != EOF )
-        cout << g;
-    // close the file
-    fclose(fp);
-}
 
+    // once we hit the end of the file we are marked as "failed", so clear that
+    // then "seek" to the beginning of the file to start again
+    file.clear(); // Clears the _state_ of the file, not its contents!
+    file.seekg(0);
+
+    // Read the file again, and print to the screen
+    while (file.get(g)) {
+        cout << g;
+    }
+
+    // close the file
+    file.close();
+}
