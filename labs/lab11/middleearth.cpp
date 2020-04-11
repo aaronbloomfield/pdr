3355c90950d1c145bbfa91df865e7b5d8d79cbf7
Winston Liu <Wliu1402@gmail.com>
2020-04-10 23:38:15 -0400

    Clean up middleearth.cpp/h

--- labs/lab11/middleearth.cpp
+++ labs/lab11/middleearth.cpp
@@ -1,9 +1,9 @@
 #include "middleearth.h"
 
 #include <algorithm>
-#include <stdlib.h>
-#include <math.h>
-#include <time.h>
+#include <cstdlib>
+#include <cmath>
+#include <ctime>
 
 // The list of all the place names that we'll be using
 string all_city_names[] = {
@@ -59,102 +59,126 @@ string all_city_names[] = {
 };
 
 // Iluvatar, the creator of Middle-Earth
-MiddleEarth::MiddleEarth (int xsize, int ysize, int num_cities, int seed) {
+MiddleEarth::MiddleEarth(int xsize, int ysize, int num_cities, int seed) {
     // set up the random number seed
-    srand( (seed==-1) ? time(NULL) : seed );
+    srand(seed == -1 ? time(NULL) : seed);
+
     // count the number of cities in the array
-    for ( num_city_names = 0; all_city_names[num_city_names] != "END";
-            num_city_names++ );
-    if ( num_cities > num_city_names ) {
+    this->num_city_names = 0;
+    while (all_city_names[num_city_names] != "END") {
+        num_city_names++;
+    }
+
+    if (num_cities > num_city_names) {
         cout << "There are only " << num_city_names << " city names, so "
              << num_cities << " cities cannot be created.\nExiting." << endl;
         exit(0);
     }
-    if ( num_cities < 5 )
+
+    if (num_cities < 5) {
         num_cities = 5;
-    // select the num_cities names of the cities that we'll be using
-    for ( int i = 0; all_city_names[i] != "END"; i++ )
-        cities.push_back(string(all_city_names[i]));
-    random_shuffle(cities.begin(), cities.end());
-    cities.erase(cities.begin()+num_cities,cities.end());
-    // compute random city positions
-    for ( unsigned int i = 0; i < cities.size(); i++ ) {
-        xpos.push_back((rand()/((float)RAND_MAX)) * xsize);
-        ypos.push_back((rand()/((float)RAND_MAX)) * ysize);
     }
+
+    // add all the cities into a vector
+    for (int i = 0; all_city_names[i] != "END"; i++) {
+        cities.push_back(all_city_names[i]);
+    }
+
+    random_shuffle(cities.begin(), cities.end()); // shuffle all the cities
+    cities.erase(cities.begin() + num_cities, cities.end()); // then remove the ones we won't be using
+
+    // compute random city positions
+    for (auto i = 0; i < cities.size(); i++) {
+        xpos.push_back((rand() / (float) RAND_MAX) * xsize);
+        ypos.push_back((rand() / (float) RAND_MAX) * ysize);
+    }
+
     // compute the 2-d distance array
     this->xsize = xsize;
     this->ysize = ysize;
-    // we assume that num_cities < xsize*ysize
-    distances = new float[num_cities*num_cities]; // row-major order
-    for ( int r = 0; r < num_cities; r++ )
-        for ( int c = 0; c < num_cities; c++ ) {
-            distances[r*num_cities+c] = sqrt((xpos[c]-xpos[r])*(xpos[c]-xpos[r]) +
-                                             (ypos[c]-ypos[r])*(ypos[c]-ypos[r]));
+
+    // we assume that num_cities < xsize * ysize
+    distances = new float[num_cities * num_cities]; // row-major order
+    for (int r = 0; r < num_cities; r++) {
+        for (int c = 0; c < num_cities; c++) {
+            distances[r * num_cities + c] = sqrt((xpos[c] - xpos[r]) * (xpos[c] - xpos[r]) +
+                                                 (ypos[c] - ypos[r]) * (ypos[c] - ypos[r]));
         }
+    }
+
     // create hash of indices so we don't have to do a linear search
     // each time we want to find a city name to index mapping
-    for ( unsigned int i = 0; i < cities.size(); i++ )
+    for (auto i = 0; i < cities.size(); i++) {
         indices[cities[i]] = i;
+    }
 }
 
 // Sauron, the destructor of Middle-Earth.
-MiddleEarth::~MiddleEarth () {
+MiddleEarth::~MiddleEarth() {
     delete[] distances;
 }
 
-// The Mouth of Sauron!  (prints out info on the created 'world')
+// The Mouth of Sauron!
+// Prints out info on the created 'world'
 void MiddleEarth::print() {
     cout << "there are " << num_city_names
          << " locations to choose from; we are using " << cities.size() << endl;
     cout << "they are: " << endl;
-    for ( unsigned int i = 0; i < cities.size(); i++ )
+    for (auto i = 0; i < cities.size(); i++) {
         cout << "\t" << cities[i] << " @ (" << xpos[i] << ", " << ypos[i]
              << ")" << endl;
+    }
 }
 
-// prints a tab-separated table of the distances (this can be loaded
-// into Excel or similar)
+// Prints a tab-separated table of the distances,
+// which can be loaded into Excel or similar
 void MiddleEarth::printTable() {
     cout << "Table: " << endl << endl << "Location\txpos\typos\t";
-    for ( unsigned int r = 0; r < cities.size(); r++ )
+    for (auto r = 0; r < cities.size(); r++) {
         cout << cities[r] << "\t";
+    }
     cout << endl;
-    for ( unsigned int r = 0; r < cities.size(); r++ ) {
+
+    for (auto r = 0; r < cities.size(); r++) {
         cout << cities[r] << "\t" << xpos[r] << "\t" << ypos[r] << "\t";
-        for ( unsigned int c = 0; c < cities.size(); c++ )
-            cout << distances[r*cities.size()+c] << "\t";
+        for (auto c = 0; c < cities.size(); c++) {
+            cout << distances[r * cities.size() + c] << "\t";
+        }
         cout << endl;
     }
 }
 
-// This method returns the distance between the two passed cities.  If
-// we assume that the hash table (i.e. the map) is O(1), then this
-// method call is also O(1)
-float MiddleEarth::getDistance (string city1, string city2) {
-    return distances[indices[city1]*cities.size()+indices[city2]];
+// This method returns the distance between the two passed cities.
+// If we assume that the hash table (i.e. the map) is O(1),
+// then this method call is also O(1)
+float MiddleEarth::getDistance(string city1, string city2) {
+    return distances[indices[city1] * cities.size() + indices[city2]];
 }
 
-// Returns the list of cities to travel to.  The first city is the
-// original start point as well as the end point.  The number of
-// cities passed in does not include this start/end point (so there
-// will be length+1 entries in the returned vector).
-vector<string> MiddleEarth::getItinerary (unsigned int length) {
-    length++; // to account for the start point
+// Returns the list of cities to travel to.
+// The first city is the original start point as well as the end point.
+// The number of cities passed in does not include this start/end point
+// (so there will be length+1 entries in the returned vector).
+vector<string> MiddleEarth::getItinerary(unsigned int length) {
     // check parameter
-    if ( length > cities.size() ) {
-        cout << "You have requested an itinerary of " << length-1
+    if (length >= cities.size()) {
+        cout << "You have requested an itinerary of " << length
              << " cities; you cannot ask for an itinerary of more than length "
-             << cities.size()-1 << endl;
+             << cities.size() - 1 << endl;
         exit(0);
     }
+
+    length++; // to account for the start point
+
     // we need to make a deep copy of the cities vector.  itinerary is a
     // pointer so that it doesn't get deleted when it goes out of scope.
     vector<string> itinerary;
-    for ( unsigned int i = 0; i < cities.size(); i++ )
+    for (auto i = 0; i < cities.size(); i++) {
         itinerary.push_back(cities[i]);
+    }
+
     // shuffle, erase unneeded ones, and return the itinerary
     random_shuffle(itinerary.begin(), itinerary.end());
-    itinerary.erase(itinerary.begin()+length,itinerary.end());
+    itinerary.erase(itinerary.begin() + length, itinerary.end());
     return itinerary;
 }
--- labs/lab11/middleearth.h
+++ labs/lab11/middleearth.h
@@ -1,30 +1,30 @@
+#ifndef MIDDLEEARTH_H
+#define MIDDLEEARTH_H
+
 #include <iostream>
 #include <vector>
 #include <string>
 #include <map>
-
-#ifndef MIDDLEEARTH_H
-#define MIDDLEEARTH_H
 
 using namespace std;
 
 // see the comments in the lab11 write-up, or in middleearth.cpp
 
 class MiddleEarth {
+public:
+    MiddleEarth(int xsize, int ysize, int numcities, int seed);
+    ~MiddleEarth();
+    void print();
+    void printTable();
+    float getDistance(string city1, string city2);
+    vector<string> getItinerary(unsigned int length);
+
 private:
     int num_city_names, xsize, ysize;
     vector<float> xpos, ypos;
     vector<string> cities;
-    float *distances;
+    float* distances;
     map<string, int> indices;
-
-public:
-    MiddleEarth (int xsize, int ysize, int numcities, int seed);
-    ~MiddleEarth();
-    void print();
-    void printTable();
-    float getDistance (string city1, string city2);
-    vector<string> getItinerary(unsigned int length);
 };
 
 #endif
