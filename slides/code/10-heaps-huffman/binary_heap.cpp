add0a2bee740c69a85b89bd96b820853345cb544
Winston Liu <Wliu1402@gmail.com>
2020-04-16 16:59:08 -0400

    Clean up binary_heap

--- slides/code/10-heaps-huffman/binary_heap.cpp
+++ slides/code/10-heaps-huffman/binary_heap.cpp
@@ -16,8 +16,9 @@ binary_heap::binary_heap(vector<int> vec) : heap_size(vec.size()) {
     heap = vec;
     heap.push_back(heap[0]);
     heap[0] = 0;
-    for ( int i = heap_size/2; i > 0; i-- )
+    for (int i = heap_size/2; i > 0; i--) {
         percolateDown(i);
+    }
 }
 
 // the destructor doesn't need to do much
@@ -36,16 +37,19 @@ void binary_heap::percolateUp(int hole) {
     int x = heap[hole];
     // while we haven't run off the top and while the
     // value is less than the parent...
-    for ( ; (hole > 1) && (x < heap[hole/2]); hole /= 2 )
+    for ( ; (hole > 1) && (x < heap[hole/2]); hole /= 2) {
         heap[hole] = heap[hole/2]; // move the parent down
+    }
     // correct position found!  insert at that spot
     heap[hole] = x;
 }
 
 int binary_heap::deleteMin() {
     // make sure the heap is not empty
-    if ( heap_size == 0 )
+    if (heap_size == 0) {
         throw "deleteMin() called on empty heap";
+    }
+
     // save the value to be returned
     int ret = heap[1];
     // move the last inserted position into the root
@@ -62,25 +66,28 @@ void binary_heap::percolateDown(int hole) {
     // get the value to percolate down
     int x = heap[hole];
     // while there is a left child...
-    while ( hole*2 <= heap_size ) {
+    while (hole*2 <= heap_size) {
         int child = hole*2; // the left child
         // is there a right child?  if so, is it lesser?
-        if ( (child+1 <= heap_size) && (heap[child+1] < heap[child]) )
+        if ((child+1 <= heap_size) && (heap[child+1] < heap[child])) {
             child++;
+        }
         // if the child is greater than the node...
-        if ( x > heap[child] ) {
+        if (x > heap[child]) {
             heap[hole] = heap[child]; // move child up
             hole = child;             // move hole down
-        } else
+        } else {
             break;
+        }
     }
     // correct position found!  insert at that spot
     heap[hole] = x;
 }
 
 int binary_heap::findMin() {
-    if ( heap_size == 0 )
+    if (heap_size == 0) {
         throw "findMin() called on empty heap";
+    }
     return heap[1];
 }
 
@@ -99,12 +106,13 @@ bool binary_heap::isEmpty() {
 
 void binary_heap::print() {
     cout << "(" << heap[0] << ") ";
-    for ( int i = 1; i <= heap_size; i++ ) {
+    for (int i = 1; i <= heap_size; i++) {
         cout << heap[i] << " ";
         // next line from http://tinyurl.com/mf9tbgm
         bool isPow2 = (((i+1) & ~(i))==(i+1))? i+1 : 0;
-        if ( isPow2 )
+        if (isPow2) {
             cout << endl << "\t";
+        }
     }
     cout << endl;
 }
